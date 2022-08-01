80751ad507161269597dbe1b98b3809a57a0d1ec
Mario Lučki <luckimario@gmail.com>
2022-08-01 20:47:33 +0200

    feat: cleanup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,45 @@
+import {
+	getChainOptions,
+	StaticWalletProvider,
+	WalletControllerChainOptions,
+	WalletProvider,
+} from '@terra-money/wallet-provider'
+import { AppProps } from 'next/app'
+import Link from 'next/link'
+import React from 'react'
+
+export default function App({
+	Component,
+	defaultNetwork,
+	walletConnectChainIds,
+}: AppProps & WalletControllerChainOptions) {
+	const main = (
+		<main>
+			<header style={{ display: 'flex', gap: '1em' }}>
+				<Link href='/'>Main</Link>
+			</header>
+
+			<Component />
+		</main>
+	)
+
+	return typeof window !== 'undefined' ? (
+		<WalletProvider
+			defaultNetwork={defaultNetwork}
+			walletConnectChainIds={walletConnectChainIds}
+		>
+			{main}
+		</WalletProvider>
+	) : (
+		<StaticWalletProvider defaultNetwork={defaultNetwork}>
+			{main}
+		</StaticWalletProvider>
+	)
+}
+
+App.getInitialProps = async () => {
+	const chainOptions = await getChainOptions()
+	return {
+		...chainOptions,
+	}
+}
