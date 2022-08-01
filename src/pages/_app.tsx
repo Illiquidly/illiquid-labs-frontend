5003b34a3b87712a29ea472616458835a153dc1c
Mario Lučki <luckimario@gmail.com>
2022-08-01 21:30:46 +0200

    feat: add rebass, cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,20 +7,24 @@ import {
 import { AppProps } from 'next/app'
 import Link from 'next/link'
 import React from 'react'
+import { theme } from 'src/components/theme'
+import { ThemeProvider } from 'styled-components'
 
 export default function App({
 	Component,
 	defaultNetwork,
 	walletConnectChainIds,
+	pageProps,
 }: AppProps & WalletControllerChainOptions) {
 	const main = (
-		<main>
-			<header style={{ display: 'flex', gap: '1em' }}>
-				<Link href='/'>Main</Link>
-			</header>
-
-			<Component />
-		</main>
+		<ThemeProvider theme={theme}>
+			<main>
+				<header style={{ display: 'flex', gap: '1em' }}>
+					<Link href='/'>Main</Link>
+				</header>
+				<Component {...pageProps} />
+			</main>
+		</ThemeProvider>
 	)
 
 	return typeof window !== 'undefined' ? (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import { Button, Flex } from 'rebass/styled-components'
+
 export default function Index() {
-	return <div />
+	return (
+		<div>
+			<Flex>
+				<Button variant='primary' mr={2}>
+					Beep
+				</Button>
+				<Button variant='secondary'>Boop</Button>
+			</Flex>
+		</div>
+	)
 }
