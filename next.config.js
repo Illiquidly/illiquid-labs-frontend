2b14420221518814888c8b1aa3cbb6a9cb38c478
Mario Lučki <luckimario@gmail.com>
2022-08-06 22:08:01 +0200

    fix (nexjs): fix build errors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+	images: {
+		loader: 'akamai',
+		path: '',
+	},
+	pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/,
