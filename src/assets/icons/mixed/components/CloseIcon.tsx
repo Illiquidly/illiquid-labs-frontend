ffbc3ecf501c9da18e888dfbd9babdbb2a04ee42
Mario Lučki <luckimario@gmail.com>
2022-08-19 19:12:19 +0200

    fix (eslint): eslint corrections

--- src/assets/icons/mixed/components/CloseIcon.tsx
+++ src/assets/icons/mixed/components/CloseIcon.tsx
@@ -1,25 +1,25 @@
-import * as React from "react"
+import * as React from 'react'
 
 function CloseIcon(props) {
-  return (
-    <svg
-      width={12}
-      height={12}
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path
-        d="M1 11L11 1M11 11L1 1"
-        stroke="#fff"
-        strokeWidth={2}
-        strokeMiterlimit={10}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  )
+	return (
+		<svg
+			width={12}
+			height={12}
+			viewBox='0 0 12 12'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+			{...props}
+		>
+			<path
+				d='M1 11L11 1M11 11L1 1'
+				stroke='#fff'
+				strokeWidth={2}
+				strokeMiterlimit={10}
+				strokeLinecap='round'
+				strokeLinejoin='round'
+			/>
+		</svg>
+	)
 }
 
 export default CloseIcon
--- src/assets/icons/mixed/components/CloseIconSmall.tsx
+++ src/assets/icons/mixed/components/CloseIconSmall.tsx
@@ -1,23 +1,23 @@
-import * as React from "react"
+import * as React from 'react'
 
 function CloseIconSmall(props) {
-  return (
-    <svg
-      width={10}
-      height={10}
-      viewBox="0 0 10 10"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M.438.439a.68.68 0 01.962 0l3.6 3.6L8.6.44a.68.68 0 11.963.962L5.963 5l3.6 3.6a.68.68 0 11-.962.963L5 5.964 1.4 9.563a.68.68 0 01-.962-.962l3.6-3.6L.438 1.4a.68.68 0 010-.962z"
-        fill="#fff"
-      />
-    </svg>
-  )
+	return (
+		<svg
+			width={10}
+			height={10}
+			viewBox='0 0 10 10'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+			{...props}
+		>
+			<path
+				fillRule='evenodd'
+				clipRule='evenodd'
+				d='M.438.439a.68.68 0 01.962 0l3.6 3.6L8.6.44a.68.68 0 11.963.962L5.963 5l3.6 3.6a.68.68 0 11-.962.963L5 5.964 1.4 9.563a.68.68 0 01-.962-.962l3.6-3.6L.438 1.4a.68.68 0 010-.962z'
+				fill='#fff'
+			/>
+		</svg>
+	)
 }
 
 export default CloseIconSmall
