7b0c5d4b20a1da24bfe8eec08a567c7bf840b9e9
Kayanski <kowalski.kowalskin@gmail.com>
2022-08-19 16:52:49 +0200

    Added Trade creation Mobile Icons from figma

---
+++ src/assets/icons/mixed/index.ts
@@ -0,0 +1,5 @@
+export { default as CloseIcon } from './components/CloseIcon'
+export { default as CloseIconSmall } from './components/CloseIconSmall'
+export { default as LunaIcon } from './components/LunaIcon'
+export { default as WalletIcon } from './components/WalletIcon'
+
