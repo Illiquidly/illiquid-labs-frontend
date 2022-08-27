c7fea7fbbc9a90c63751bcbb9d1d0b1b4ec564e7
Marino Kolarić <marino.kolaric@cinnamon.agency>
2022-08-27 17:45:15 +0200

    Feat: add background svgs

---
+++ src/assets/images/TradeBackgroundBlob.tsx
@@ -0,0 +1,35 @@
+function TradeBackgroundBlob(props) {
+	return (
+		<svg
+			width='667'
+			height='675'
+			viewBox='0 0 667 675'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+			{...props}
+		>
+			<path
+				opacity='0.5'
+				fillRule='evenodd'
+				clipRule='evenodd'
+				d='M325.286 108.926C360.561 120.685 401.642 70.4078 433.279 89.9445C465.239 109.68 447.067 163.31 465.942 195.785C484.902 228.405 519.985 246.35 541.999 276.992C587.873 340.844 680.545 396.179 665.332 473.315C652.402 538.876 544.296 522.685 486.82 556.774C428.435 591.402 393.167 675.455 325.286 674.998C257.309 674.541 222.071 591.543 165.002 554.609C111.563 520.024 5.58014 528.075 0.257436 464.645C-6.40611 385.236 118.296 353.164 141.705 276.992C155.465 232.22 94.1432 192.556 98.2162 145.895C102.937 91.8147 112.234 9.50819 165.816 0.784702C230.306 -9.71499 263.3 88.2624 325.286 108.926Z'
+				fill='url(#paint0_linear_2035_6475)'
+			/>
+			<defs>
+				<linearGradient
+					id='paint0_linear_2035_6475'
+					x1='66.5'
+					y1='-50'
+					x2='498.5'
+					y2='665.5'
+					gradientUnits='userSpaceOnUse'
+				>
+					<stop offset='0.0974578' stopColor='white' stopOpacity='0.1' />
+					<stop offset='1' stopColor='white' stopOpacity='0' />
+				</linearGradient>
+			</defs>
+		</svg>
+	)
+}
+
+export default TradeBackgroundBlob
