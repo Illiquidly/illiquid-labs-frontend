962e01184febf936ced7fc5befc38252e107920a
Marino Kolarić <marino.kolaric@cinnamon.agency>
2022-08-28 11:31:32 +0200

    Fix: issue with non applied default value

--- src/components/ui/layout/LayoutContainer.tsx
+++ src/components/ui/layout/LayoutContainer.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import { ThemeUIStyleObject } from 'theme-ui'
 import { Container } from './LayoutContainer.styled'
 
 interface Props {
 	children: React.ReactNode
-	sx?: ThemeUIStyleObject
 }
 
-export const LayoutContainer = ({ children, sx }: Props) => {
-	return <Container sx={sx}>{children}</Container>
-}
-
-LayoutContainer.defaultProps = {
-	sx: {},
+export const LayoutContainer = (props: Props) => {
+	const { children } = props
+	return <Container {...props}>{children}</Container>
 }
 
 export default LayoutContainer
