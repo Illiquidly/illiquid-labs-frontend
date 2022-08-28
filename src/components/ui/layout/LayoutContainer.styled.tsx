358ffe6ff247acf4c3d0399b7af09536a0b780c2
Marino Kolarić <marino.kolaric@cinnamon.agency>
2022-08-28 10:50:14 +0200

    Feat: define style for LayoutContainer

--- src/components/ui/layout/LayoutContainer.styled.tsx
+++ src/components/ui/layout/LayoutContainer.styled.tsx
@@ -3,4 +3,14 @@ import { Box } from 'theme-ui'
 
 export const Container = styled(Box)`
 	/* To be defined -> there are too many of them */
+	position: relative;
+	max-width: 1272px; /* 1240 width from design + 32px padding */
+	width: 100%;
+	margin: 0 auto;
 `
+
+Container.defaultProps = {
+	sx: {
+		p: ['12px 16px', '32px', '32px 16px'],
+	},
+}
--- src/components/ui/layout/LayoutContainer.tsx
+++ src/components/ui/layout/LayoutContainer.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { ThemeUIStyleObject } from 'theme-ui'
 import { Container } from './LayoutContainer.styled'
 
 interface Props {
 	children: React.ReactNode
+	sx?: ThemeUIStyleObject
 }
 
-export const LayoutContainer = ({ children }: Props) => {
-	return <Container>{children}</Container>
+export const LayoutContainer = ({ children, sx }: Props) => {
+	return <Container sx={sx}>{children}</Container>
+}
+
+LayoutContainer.defaultProps = {
+	sx: {},
 }
 
 export default LayoutContainer
