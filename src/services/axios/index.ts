0a3eed4063fd03234ff8a0bfe0efbd9eb5a30277
Mario Lučki <luckimario@gmail.com>
2022-08-29 18:03:29 +0200

    fix (api): replace api endpoint

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -1,13 +1,7 @@
 import client from 'axios'
 
-export const baseURL = 'https://api.illiquidly.io:8443/'
+export const baseURL = 'https://api.illiquidly.io/'
 
 export const axios = client.create({
 	baseURL,
-})
-
-export const migratorURL = 'https://api.illiquidly.io:8444/'
-
-export const migratorClient = client.create({
-	baseURL: migratorURL,
 })
