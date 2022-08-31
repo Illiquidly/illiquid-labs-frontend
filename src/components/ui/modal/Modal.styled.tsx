7ea7732be8ac5156986dd2ea97b27962d30d5954
Marino Kolarić <marino.kolaric@cinnamon.agency>
2022-08-31 22:59:29 +0200

    Refactor: main Modal HOC

---
+++ src/components/ui/modal/Modal.styled.tsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled'
+import { HEADER_HEIGHT } from 'constants/components'
+import { Box } from 'theme-ui'
+
+export const ModalWrapper = styled.div<{ isActive: boolean }>`
+	display: ${props => (props.isActive ? 'block' : 'none')};
+	z-index: ${props => props.theme.zIndices.modal};
+	width: 100%;
+	height: 100%;
+	position: fixed;
+`
+
+export const ModalContainer = styled(Box)`
+	position: absolute;
+	top: ${HEADER_HEIGHT};
+	left: 50%;
+	transform: translateX(-50%);
+	min-width: 310px;
+	overflow: auto;
+	max-height: calc(100vh - ${HEADER_HEIGHT});
+`
+
+ModalContainer.defaultProps = {
+	sx: {},
+}
--- src/components/ui/modal/Modal.tsx
+++ src/components/ui/modal/Modal.tsx
@@ -1,121 +1,30 @@
-import ReactModal from 'react-modal'
-import styled from '@emotion/styled'
-import { Box, Flex, IconButton, ThemeUIStyleObject } from 'theme-ui'
-import { ModalCloseIcon } from 'assets/icons/modal'
-import React from 'react'
-import { noop } from 'lodash'
-import { useTheme } from '@emotion/react'
-import { OnlyMobileAndTablet } from '../layout/Layout'
-
-const StyledReactModal = styled(ReactModal)`
-	flex-direction: column;
-	display: flex;
-	position: fixed;
-	top: 0px;
-	left: 0px;
-	right: 0px;
-	bottom: 0px;
-	background-color: transparent;
-
-	& .overlay: {
-		background-color: transparent;
-	}
-`
-
-const ModalTitle = styled(Box)`
-	font-family: 'Inter';
-	font-style: normal;
-	font-weight: 700;
-`
-
-ModalTitle.defaultProps = {
-	sx: {
-		fontSize: ['26px', '26px', '34px'],
-		lineHeight: ['36px', '36px', '48px'],
-	},
-}
+import React, { useRef } from 'react'
+import { ThemeUIStyleObject } from 'theme-ui'
+import { useOnClickOutside } from 'utils/react/useOnClickOutside'
+import { ModalContainer, ModalWrapper } from './Modal.styled'
 
 export interface ModalProps {
-	title?: string
-	isOpen: boolean
-	onAfterOpen?: () => void
-	onRequestClose?: () => void
-	children?: React.ReactNode
+	isActive: boolean
+	children: React.ReactNode
+	closeModal: () => void
 	sx?: ThemeUIStyleObject
 }
 
-const Modal = ({
-	isOpen,
-	onAfterOpen,
-	onRequestClose,
-	title,
+const Modal: React.FunctionComponent<ModalProps> = ({
+	isActive = false,
+	closeModal,
 	children,
-	sx,
-}: ModalProps) => {
-	const theme = useTheme()
-	return (
-		<StyledReactModal
-			{...{
-				isOpen,
-				onAfterOpen,
-				onRequestClose,
-			}}
-			overlayClassName='overlay'
-		>
-			<Flex
-				sx={{
-					position: 'fixed',
-					inset: 0,
-					flex: 1,
-					flexDirection: 'column',
-					bg: 'rgba(0, 0, 0, 0.7)',
-				}}
-			>
-				<Flex
-					sx={{
-						display: ['none', 'none', 'flex'],
-						height: '72px',
-						p: '24px 16px',
-						justifyContent: 'flex-end',
-						alignItems: 'center',
-					}}
-				>
-					<IconButton size='40px' onClick={onRequestClose}>
-						<ModalCloseIcon />
-					</IconButton>
-				</Flex>
-				<Flex
-					sx={{
-						bg: 'dark100',
-						flexDirection: 'column',
-						flex: 1,
-						p: ['16px', '25px 33.5px', '32px 112px'],
-						borderRadius: [0, 0, '16px 16px 0px 0px'],
-						...sx,
-					}}
-				>
-					<Flex sx={{ flexDirection: 'column', flex: 1, overflow: 'hidden' }}>
-						<Flex sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-							<ModalTitle>{title}</ModalTitle>
-							<OnlyMobileAndTablet>
-								<IconButton size='40px' onClick={onRequestClose}>
-									<ModalCloseIcon fill={theme.colors.dark500} />
-								</IconButton>
-							</OnlyMobileAndTablet>
-						</Flex>
-						{children}
-					</Flex>
-				</Flex>
-			</Flex>
-		</StyledReactModal>
-	)
-}
+}) => {
+	const containerRef = useRef(null)
+	useOnClickOutside(containerRef, closeModal)
 
-Modal.defaultProps = {
-	title: '',
-	onAfterOpen: noop,
-	onRequestClose: noop,
-	sx: {},
+	if (!isActive) return null
+
+	return (
+		<ModalWrapper isActive={isActive}>
+			<ModalContainer ref={containerRef}>{children}</ModalContainer>
+		</ModalWrapper>
+	)
 }
 
 export default Modal
