70008d98aea3bdbf330d50dd9c2206c57065ff86
Mario Lučki <luckimario@gmail.com>
2022-09-02 07:58:19 +0200

    fix (switch): fix hover

--- src/components/ui/switch/Switch.styled.ts
+++ src/components/ui/switch/Switch.styled.ts
@@ -16,6 +16,7 @@ export const Input = styled.input`
 `
 
 export const ToggleBackground = styled.div<{ checked?: boolean }>`
+	border: 1px solid transparent;
 	width: 36px;
 	height: 20px;
 	border-radius: 15.5px;
@@ -23,8 +24,15 @@ export const ToggleBackground = styled.div<{ checked?: boolean }>`
 		props.checked ? props.theme.colors.primary100 : props.theme.colors.dark300};
 	display: flex;
 	justify-content: start;
+	align-items: center;
 	padding: 2px;
 	box-sizing: border-box;
+
+	&:hover {
+		border: 1px solid ${props => props.theme.colors.primary600};
+
+		box-shadow: 0px 0px 0px 4px rgba(63, 138, 224, 0.3);
+	}
 `
 
 export const Knob = styled.div<{ checked?: boolean }>`
