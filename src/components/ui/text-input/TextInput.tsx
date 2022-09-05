265b3453d24b15366badc0be10aa4795529a1f7c
Mario Lučki <luckimario@gmail.com>
2022-09-05 22:02:34 +0200

    fix (text-input): correct input styles a bit

--- src/components/ui/text-input/TextInput.tsx
+++ src/components/ui/text-input/TextInput.tsx
@@ -7,7 +7,7 @@ export interface TextInputProps
 }
 
 const TextInputStyled = styled.input<TextInputProps>`
-	border: ${props => (props.error ? 1.5 : 1)}px solid
+	border: 1.5px solid
 		${props =>
 			props.error ? props.theme.colors.error100 : props.theme.colors.dark500};
 	padding-inline: 14px;
@@ -15,8 +15,6 @@ const TextInputStyled = styled.input<TextInputProps>`
 	background: ${props => props.theme.colors.dark400};
 	color: ${props => props.theme.colors.natural50};
 	border-radius: 8px;
-	margin: ${props => (props.error ? 0 : '0.5px')};
-	filter: drop-shadow(0px 1px 2px rgba(16, 24, 40, 0.05));
 	&::placeholder {
 		color: ${props => props.theme.colors.gray600};
 		opacity: 1;
@@ -35,10 +33,9 @@ const TextInputStyled = styled.input<TextInputProps>`
 			}`};
 	}
 	&:focus,
-	active {
+	&:active {
 		outline: none;
 		margin: 0;
-		filter: drop-shadow(0px 0px 2px rgba(63, 138, 224, 0.18));
 		border: ${props =>
 			`1.5px solid ${
 				props.error ? props.theme.colors.error100 : props.theme.colors.primary100
@@ -49,9 +46,9 @@ const TextInputStyled = styled.input<TextInputProps>`
 
 export const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
 	(props, ref) => {
-		const { children } = props
+		const { children, ...rest } = props
 		return (
-			<TextInputStyled {...props} ref={ref}>
+			<TextInputStyled {...rest} ref={ref}>
 				{children}
 			</TextInputStyled>
 		)
