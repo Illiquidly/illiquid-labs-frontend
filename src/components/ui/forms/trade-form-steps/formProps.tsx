892ed6b67c059224d049331936675c4e1d3c3ab8
Marino Kolarić <marino.kolaric@cinnamon.agency>
2022-09-07 21:04:36 +0200

    Feat: add enum

--- src/components/ui/forms/trade-form-steps/formProps.tsx
+++ src/components/ui/forms/trade-form-steps/formProps.tsx
@@ -1,5 +1,11 @@
 import { NFT } from 'services/api/walletNFTsService'
 
+export enum COLLECTION_TYPE {
+	SPECIFIC = 'SPECIFIC',
+	ANY = 'ANY',
+}
+
 export interface TradeFormStepsProps {
 	selectedNFTs: NFT[]
+	collectionType: COLLECTION_TYPE
 }
