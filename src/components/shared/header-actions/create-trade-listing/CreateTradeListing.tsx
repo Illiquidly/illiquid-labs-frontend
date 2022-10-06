aef70e550566de30d858e2b1fb3a28198915ebac
Mario Lučki <luckimario@gmail.com>
2022-10-06 09:29:18 +0200

    fix (header actions): fix show only create trade listing when connected

--- src/components/shared/header-actions/create-trade-listing/CreateTradeListing.tsx
+++ src/components/shared/header-actions/create-trade-listing/CreateTradeListing.tsx
@@ -5,17 +5,22 @@ import { Box, Flex } from 'theme-ui'
 import * as ROUTES from 'constants/routes'
 import { ConnectButton } from 'components/shared/connect-button'
 import { useTranslation } from 'next-i18next'
+import { useWallet, WalletStatus } from '@terra-money/use-wallet'
 
 export default function CreateTradeListing() {
 	const { t } = useTranslation(['common'])
+	const wallet = useWallet()
 	return (
 		<Flex sx={{ gap: '8px', height: '40px' }}>
-			<Button variant='gradient' size='medium' href={ROUTES.TRADE_CREATE_LISTING}>
-				<CreateListingAddIcon />
-				<Box sx={{ display: ['none', 'block'], ml: '8px' }}>
-					{t('common:create-listing')}
-				</Box>
-			</Button>
+			{wallet.status === WalletStatus.WALLET_CONNECTED && (
+				<Button variant='gradient' size='medium' href={ROUTES.TRADE_CREATE_LISTING}>
+					<CreateListingAddIcon />
+
+					<Box sx={{ display: ['none', 'block'], ml: '8px' }}>
+						{t('common:create-listing')}
+					</Box>
+				</Button>
+			)}
 			<ConnectButton />
 		</Flex>
 	)
