bf34258d48b45a25c859542b0685048d5ee3d26c
Mario Lučki <luckimario@gmail.com>
2022-10-16 18:30:49 +0200

    feat (contracts): update contracts for mainnet

--- src/services/blockchain/addresses.ts
+++ src/services/blockchain/addresses.ts
@@ -21,8 +21,10 @@ const addresses: { [k: string]: { [key: string]: string } } = {
 		...cw20Addresses['columbus-5'],
 	},
 	'phoenix-1': {
-		'p2p-trade': '',
-		'fee-collector': '',
+		'p2p-trade':
+			'terra1d3xtfkumcxl6225mhfvgwjt0sql30x5yc0hrap2qdfwfs762aj5sa2we47',
+		'fee-collector':
+			'terra1f0u8h08a8eyvyg770tkcln6l78080kafrmnlw0f5qd89qcydqv6s0y0wcc',
 		...cw20Addresses['phoenix-1'],
 	},
 	'pisco-1': {
