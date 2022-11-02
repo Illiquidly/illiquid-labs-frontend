7f35f800f9d4592e6cd0b0c33319a62cc4544bdf
Mario Lučki <luckimario@gmail.com>
2022-11-02 18:04:13 +0100

    feat (buy raffle review modal): add raffle review modal

---
+++ src/components/raffle-listing-details/modals/buy-raffle-review-modal/BuyRaffleReviewModal.tsx
@@ -0,0 +1,145 @@
+import React from 'react'
+import { Box, Flex, IconButton } from 'theme-ui'
+import { useTranslation } from 'next-i18next'
+
+import NiceModal, { useModal } from '@ebay/nice-modal-react'
+import { useTheme } from '@emotion/react'
+
+import { ModalCloseIcon, ModalSuccessCircleIcon } from 'assets/icons/modal'
+
+import { Button, Modal } from 'components/ui'
+
+import { ModalLayoutContainer } from 'components/layout'
+import { Raffle } from 'services/api/rafflesService'
+import moment from 'moment'
+import {
+	ModalBody,
+	ModalContainer,
+	ModalHeader,
+	ModalContent,
+	Title,
+	Subtitle,
+	DescriptionCard,
+	DescriptionCardLabel,
+	DescriptionCardContent,
+} from './BuyRaffleReviewModal.styled'
+
+export interface BuyRaffleReviewModalProps {
+	raffle: Raffle
+	ticketNumber: number
+}
+
+const BuyRaffleReviewModal = NiceModal.create(
+	({ raffle, ticketNumber }: BuyRaffleReviewModalProps) => {
+		const modal = useModal()
+
+		const { t } = useTranslation(['common', 'raffle-listings'])
+
+		const theme = useTheme()
+
+		const endsIn = moment(raffle?.raffleInfo?.raffleOptions?.raffleStartDate).add(
+			raffle?.raffleInfo?.raffleOptions?.raffleDuration ?? 0,
+			'seconds'
+		)
+
+		return (
+			<Modal isOverHeader isOpen={modal.visible} onCloseModal={modal.remove}>
+				<ModalContainer>
+					<ModalLayoutContainer>
+						<ModalContent>
+							<ModalHeader>
+								{t('raffle-listings:buy-raffle-review-modal.title')}
+								<IconButton
+									sx={{
+										borderRadius: '32px',
+										backgroundColor: theme.colors.dark500,
+									}}
+									onClick={modal.remove}
+								>
+									<ModalCloseIcon />
+								</IconButton>
+							</ModalHeader>
+							<ModalBody>
+								<Flex sx={{ gap: '8px' }}>
+									<Box sx={{ width: '32px', height: '32px' }}>
+										<ModalSuccessCircleIcon />
+									</Box>
+									<Box>
+										<Title>
+											{t('raffle-listings:buy-raffle-review-modal.removal-successful')}
+										</Title>
+										<Subtitle>
+											{t(
+												'raffle-listings:buy-raffle-review-modal.create-listing-suggestion'
+											)}
+										</Subtitle>
+									</Box>
+								</Flex>
+								<Flex sx={{ flexDirection: 'column', gap: '12px' }}>
+									<DescriptionCard>
+										<DescriptionCardLabel>
+											{t('raffle-listings:buy-raffle-review-modal.amount-of-tickets')}
+										</DescriptionCardLabel>
+										<DescriptionCardContent>
+											{t('raffle-listings:buy-raffle-review-modal.tickets', {
+												count: ticketNumber,
+											})}
+										</DescriptionCardContent>
+									</DescriptionCard>
+
+									<DescriptionCard>
+										<DescriptionCardLabel>
+											{t('raffle-listings:buy-raffle-review-modal.raffle-start-date')}
+										</DescriptionCardLabel>
+										<DescriptionCardContent>
+											{moment(
+												raffle?.raffleInfo?.raffleOptions?.raffleStartDate
+											).toLocaleString()}
+										</DescriptionCardContent>
+									</DescriptionCard>
+
+									<DescriptionCard>
+										<DescriptionCardLabel>
+											{t('raffle-listings:buy-raffle-review-modal.ticket-cost')}
+										</DescriptionCardLabel>
+										<DescriptionCardContent>
+											{`${
+												raffle?.raffleInfo?.raffleTicketPrice?.coin?.amount ??
+												raffle?.raffleInfo?.raffleTicketPrice?.cw20Coin?.amount ??
+												0
+											} ${
+												raffle?.raffleInfo?.raffleTicketPrice?.coin?.currency ??
+												raffle?.raffleInfo?.raffleTicketPrice?.cw20Coin?.currency
+											}`}
+										</DescriptionCardContent>
+									</DescriptionCard>
+
+									<DescriptionCard>
+										<DescriptionCardLabel>
+											{t('raffle-listings:buy-raffle-review-modal.raffle-ends-in')}
+										</DescriptionCardLabel>
+										<DescriptionCardContent>{endsIn.fromNow()}</DescriptionCardContent>
+									</DescriptionCard>
+								</Flex>
+								<Flex>
+									<Button
+										variant='gradient'
+										fullWidth
+										disabled
+										onClick={() => {
+											modal.resolve()
+											modal.remove()
+										}}
+									>
+										{t('raffle-listings:buy-raffle-review-modal.review-raffle')}
+									</Button>
+								</Flex>
+							</ModalBody>
+						</ModalContent>
+					</ModalLayoutContainer>
+				</ModalContainer>
+			</Modal>
+		)
+	}
+)
+export default BuyRaffleReviewModal
