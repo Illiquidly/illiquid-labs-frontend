bffa8f494356189b6e2ac9ab1c5df41314a1eaa3
Kayanski <kowalski.kowalskin@gmail.com>
2022-11-06 23:02:24 +0100

    Added the possibility to view the website without any address connected

--- src/hooks/useAddress.ts
+++ src/hooks/useAddress.ts
@@ -2,7 +2,7 @@ import { useConnectedWallet } from '@terra-money/wallet-provider'
 
 const useAddress = () => {
 	const connectedWallet = useConnectedWallet()
-	return connectedWallet?.terraAddress ?? ''
+	return connectedWallet?.terraAddress ?? 'None'
 }
 
 export default useAddress
