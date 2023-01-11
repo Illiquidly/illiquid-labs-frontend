56c9ade9b1be3e2dbe68e41cd424cced4764d94a
Mario Lučki <luckimario@gmail.com>
2023-01-11 23:09:46 +0100

    fix (raffle): update preview modal

--- src/components/raffle-listing-details/modals/buy-raffle-review-modal/BuyRaffleReviewModal.tsx
+++ src/components/raffle-listing-details/modals/buy-raffle-review-modal/BuyRaffleReviewModal.tsx
@@ -66,7 +66,7 @@ const BuyRaffleReviewModal = NiceModal.create(
 										<DescriptionCardContent>
 											{moment(
 												raffle?.raffleInfo?.raffleOptions?.raffleStartDate
-											).toLocaleString()}
+											).fromNow()}
 										</DescriptionCardContent>
 									</DescriptionCard>
 
